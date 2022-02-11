0696180cc4335165cddc3193c418cad24d79bb79
Yu357 <forestgreen14689@gmail.com>
2022-02-11 16:48:34 +0900

    Initial Commit

---
+++ FireTodoDiary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FireTodoDiary
+//
+//  Created by Yu on 2022/02/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FireTodoDiary/FireTodoDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireTodoDiaryApp.swift
+//  FireTodoDiary
+//
+//  Created by Yu on 2022/02/11.
+//
+
+import SwiftUI
+
+@main
+struct FireTodoDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
