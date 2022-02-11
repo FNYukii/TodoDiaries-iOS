55fea86745e8001567081682d7dcfe2dd0c8ea9f
Yu357 <forestgreen14689@gmail.com>
2022-02-11 17:17:58 +0900

    Todoモデルを作成

---
+++ FireTodoDiary/Models/Todo.swift
@@ -0,0 +1,18 @@
+//
+//  Todo.swift
+//  FireTodoDiary
+//
+//  Created by Yu on 2022/02/11.
+//
+
+import Foundation
+
+struct Todo: Identifiable {
+    let id: String
+    let userId: String
+    let content: String
+    let createdAt: Date
+    let isPinned: Bool
+    let isAchieved: Bool
+    let achievedAt: Date
+}
