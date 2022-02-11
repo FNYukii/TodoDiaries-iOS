dc8f303430676c1d1d4913cbadddae2306cda418
Yu357 <forestgreen14689@gmail.com>
2022-02-11 17:14:24 +0900

    Firebaseパッケージを導入

--- FireTodoDiary/FireTodoDiaryApp.swift
+++ FireTodoDiary/FireTodoDiaryApp.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct FireTodoDiaryApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        // Connect to Firebase
+        FirebaseApp.configure()
+        
+        return true
     }
 }
