73888da6c8569d5d7869c62e8efbde62d8c4bbd6
Yu357 <forestgreen14689@gmail.com>
2022-02-14 05:34:41 +0900

    [add] Int型の年月日を、フォーマットされた文字列に変換するメソッドをDayクラスに定義

--- FireTodoDiary/Components/Day.swift
+++ FireTodoDiary/Components/Day.swift
@@ -17,4 +17,41 @@ class Day {
         let day = calendar.component(.day, from: from)
         return year * 10000 + month * 100 + day
     }
+    
+    // 20210923 -> Date
+    static func toDate(from: Int) -> Date {
+        let inputYmd = from
+        let year = inputYmd / 10000
+        let month = (inputYmd % 10000) / 100
+        let day = (inputYmd % 100)
+        let dateComponent = DateComponents(calendar: Calendar.current, year: year, month: month, day: day)
+        return dateComponent.date!
+    }
+    
+    // Date -> "2021年 10月 21日 木"
+    static func toYmdwString(from: Date) -> String {
+        let inputDate = from
+        //年月日のテキストを生成
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ja_JP")
+        dateFormatter.dateStyle = .medium
+        dateFormatter.dateFormat = "yyyy年 M月 d日"
+        let ymdText = dateFormatter.string(from: inputDate)
+        //曜日のテキストを生成
+        let calendar = Calendar(identifier: .gregorian)
+        let weekdayNumber = calendar.component(.weekday, from: inputDate)
+        let weekdaySymbolIndex: Int = weekdayNumber - 1
+        let formatter: DateFormatter = DateFormatter()
+        formatter.locale = NSLocale(localeIdentifier: "ja") as Locale
+        let weekDayText = formatter.shortWeekdaySymbols[weekdaySymbolIndex]
+        //２つのテキストを文字列連結する
+        return ymdText + " " + weekDayText
+    }
+    
+    // 20210923 -> "2021年 10月 21日 木"
+    static func toYmdwString(from: Int) -> String {
+        let date = toDate(from: from)
+        return toYmdwString(from: date)
+    }
+    
 }
