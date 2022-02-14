780c76b03980d89860db8ee724f4a4c3b40d997e
Yu357 <forestgreen14689@gmail.com>
2022-02-15 04:27:10 +0900

    [add] DaysViewModelクラスを新規作成した

---
+++ FireTodoDiary/ViewModels/DaysViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  DaysViewModel.swift
+//  FireTodoDiary
+//
+//  Created by Yu on 2022/02/15.
+//
+
+import Foundation
+
+class DaysViewModel: ObservableObject {
+    @Published var days: [Int] = []
+}
