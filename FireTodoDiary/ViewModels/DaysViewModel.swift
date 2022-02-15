c21daa7c4c961f66afacccbeb44a8f18d6be60fe
Yu357 <forestgreen14689@gmail.com>
2022-02-15 10:34:38 +0900

    [add] DaysViewModelに、最新のTodo達成日の配列を取得する処理を記述

--- FireTodoDiary/ViewModels/DaysViewModel.swift
+++ FireTodoDiary/ViewModels/DaysViewModel.swift
@@ -6,7 +6,41 @@
 //
 
 import Foundation
+import Firebase
 
 class DaysViewModel: ObservableObject {
-    @Published var days: [Int] = []
+    
+    @Published var achievedDays: [Int] = []
+    
+    init() {
+        let db = Firestore.firestore()
+        db.collection("todos")
+            .whereField("isAchieved", isEqualTo: true)
+            .order(by: "achievedAt")
+            .addSnapshotListener { querySnapshot, error in
+                guard let documents = querySnapshot?.documents else {
+                    print("HELLO! Fail! Error fetching documents: \(error!)")
+                    return
+                }
+                print("HELLO! Success! Read documents in todos")
+                
+                // 達成済みTodoの配列
+                var achievedTodos: [Todo] = []
+                documents.forEach { document in
+                    let todo = Todo(document: document)
+                    achievedTodos.append(todo)
+                }
+                
+                // Todo達成日の配列
+                var achievedDays: [Int] = []
+                for todo in achievedTodos {
+                    let achievedDay = todo.achievedDay!
+                    achievedDays.append(achievedDay)
+                }
+                // 配列から重複した要素を削除
+                achievedDays = NSOrderedSet(array: achievedDays).array as! [Int]
+                
+                self.achievedDays = achievedDays
+            }
+    }
 }
