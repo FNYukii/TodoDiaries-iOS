7a173778a3a2147b3959f709378d84ccf16f1e13
Yu357 <forestgreen14689@gmail.com>
2022-02-15 20:57:38 +0900

    [update] FourthViewのUIを実装

--- FireTodoDiary/Screens/FourthView.swift
+++ FireTodoDiary/Screens/FourthView.swift
@@ -11,13 +11,42 @@ import FirebaseAuth
 struct FourthView: View {
     var body: some View {
         NavigationView {
-            Button("signOut") {
-                do {
-                    try Auth.auth().signOut()
-                } catch {
-                    print("HELLO! Fail! Error signing out")
+            Form {
+                
+                HStack {
+                    Text("email")
+                    Spacer()
+                    Text("\("example@gmail.com")")
+                        .foregroundColor(.secondary)
                 }
+                
+                Section(header: Text("stats")) {
+                    HStack {
+                        Text("todos")
+                        Spacer()
+                        Text("5")
+                            .foregroundColor(.secondary)
+                    }
+                    HStack {
+                        Text("achievements")
+                        Spacer()
+                        Text("23")
+                            .foregroundColor(.secondary)
+                    }
+                }
+                
+                Button("signOut") {
+                    do {
+                        try Auth.auth().signOut()
+                    } catch {
+                        print("HELLO! Fail! Error signing out")
+                    }
+                }
+                .foregroundColor(.red)
+                .frame(maxWidth: .infinity, alignment: .center)
             }
+            
+            .navigationBarTitle("profile")
         }
         .navigationViewStyle(StackNavigationViewStyle())
     }
