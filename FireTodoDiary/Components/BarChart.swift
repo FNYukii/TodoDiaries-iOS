8a9d62f3482c695f30c784f24f31404017b856cd
Yu357 <forestgreen14689@gmail.com>
2022-02-17 03:51:20 +0900

    [clean] コメント追加

--- FireTodoDiary/Components/BarChart.swift
+++ FireTodoDiary/Components/BarChart.swift
@@ -15,18 +15,18 @@ struct BarChart: UIViewRepresentable {
     func makeUIView(context: Context) -> BarChartView {
         // BarChartViewを生成
         let barChartView = BarChartView()
-        barChartView.legend.enabled = false //チャートの概要非表示
+        barChartView.legend.enabled = false //チャートの概要の表示可否
         barChartView.xAxis.gridColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.3) // 縦グリッドの色
         barChartView.xAxis.gridColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.3) // 横グリッドの色
-        barChartView.xAxis.labelPosition = XAxis.LabelPosition.bottom
+        barChartView.xAxis.labelPosition = XAxis.LabelPosition.bottom // X軸ラベルの位置
         barChartView.rightAxis.enabled = false //右側のY軸目盛り非表示
         barChartView.leftAxis.axisMinimum = 0.0 //左側のY軸目盛り最小値
         barChartView.leftAxis.granularity = 1.0 //左側のY軸目盛りの区切り地
-        barChartView.doubleTapToZoomEnabled = false //ダブルタップによるズームを無効
-        barChartView.scaleXEnabled = false //X軸ピンチアウトを無効
-        barChartView.scaleYEnabled = false //Y軸ピンチアウトを無効
-        barChartView.highlightPerDragEnabled = false //ドラッグによるハイライト線表示を無効
-        barChartView.highlightPerTapEnabled = false //タップによるハイライト線表示を無効
+        barChartView.doubleTapToZoomEnabled = false //ダブルタップによるズーム
+        barChartView.scaleXEnabled = false //X軸ピンチアウト
+        barChartView.scaleYEnabled = false //Y軸ピンチアウト
+        barChartView.highlightPerDragEnabled = false //ドラッグによるハイライト線表示
+        barChartView.highlightPerTapEnabled = false //タップによるハイライト線表示
         // X軸にラベルとして表示する文字列を指定
         let dayStrings = ["1日", "2日", "3日", "4日", "5日", "6日", "7日", "８日", "9日", "10日", "11日", "12日", "13日", "14日"]
         barChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values:dayStrings)
