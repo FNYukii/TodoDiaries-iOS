e98f9312f6f75c9e994c1cd8dc96d53ebdebb835
Yu357 <forestgreen14689@gmail.com>
2022-02-17 03:22:22 +0900

    [update] BarChartをスタイリング

--- FireTodoDiary/Components/BarChart.swift
+++ FireTodoDiary/Components/BarChart.swift
@@ -15,6 +15,17 @@ struct BarChart: UIViewRepresentable {
     func makeUIView(context: Context) -> BarChartView {
         // BarChartViewを生成
         let barChartView = BarChartView()
+        barChartView.legend.enabled = false //チャートの概要非表示
+        barChartView.xAxis.gridColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.3) // 縦グリッドの色
+        barChartView.xAxis.gridColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.3) // 横グリッドの色
+        barChartView.rightAxis.enabled = false //右側のY軸目盛り非表示
+        barChartView.leftAxis.axisMinimum = 0.0 //左側のY軸目盛り最小値
+        barChartView.leftAxis.granularity = 1.0 //左側のY軸目盛りの区切り地
+        barChartView.doubleTapToZoomEnabled = false //ダブルタップによるズームを無効
+        barChartView.scaleXEnabled = false //X軸ピンチアウトを無効
+        barChartView.scaleYEnabled = false //Y軸ピンチアウトを無効
+        barChartView.highlightPerDragEnabled = false //ドラッグによるハイライト線表示を無効
+        barChartView.highlightPerTapEnabled = false //タップによるハイライト線表示を無効
         
         return barChartView
     }
@@ -31,12 +42,22 @@ struct BarChart: UIViewRepresentable {
 
         // BarChartDataSetを生成
         let barChartDataSet = BarChartDataSet(barChartDataEntries)
+        barChartDataSet.setColor(UIColor.systemBlue)
 
         // BarChartDataを生成
         let barChartData = BarChartData()
         barChartData.addDataSet(barChartDataSet)
+        barChartData.setDrawValues(false)
 
         // BarChartViewにデータをセット
         uiView.data = barChartData
+        
+        // BarChartViewのY軸ラベルの表示範囲上限を設定
+        let maxAchievedTodoCount = achievedTodoCounts.max() ?? 0
+        if(maxAchievedTodoCount > 5){
+            uiView.leftAxis.axisMaximum = Double(maxAchievedTodoCount)
+        } else {
+            uiView.leftAxis.axisMaximum = 5.0
+        }
     }
 }
