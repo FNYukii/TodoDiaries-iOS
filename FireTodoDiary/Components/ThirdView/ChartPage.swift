e9b63d462d7fbb9f3290c919e233e9d3795af7e0
Yu357 <forestgreen14689@gmail.com>
2022-02-20 23:31:21 +0900

    [update] 年内と月内のすべての達成済みTodoを読み込む処理をコメントアウトした

--- FireTodoDiary/Components/ThirdView/ChartPage.swift
+++ FireTodoDiary/Components/ThirdView/ChartPage.swift
@@ -67,19 +67,19 @@ struct ChartPage: View {
                 self.isProgressing = false
             }
         } else if unitSelection == 1 {
-            let shifetNow = Day.nowShiftedByMonth(offset: pageOffset)
-            self.pageTitle = Day.toStringUpToMonth(from: shifetNow)
-            FireTodo.readCountsOfTodoAchievedAtTheDay(readYear: shifetNow.year!, readMonth: shifetNow.month!) { value in
-                self.countsOfTodoAchieved = value
-                self.isProgressing = false
-            }
+//            let shifetNow = Day.nowShiftedByMonth(offset: pageOffset)
+//            self.pageTitle = Day.toStringUpToMonth(from: shifetNow)
+//            FireTodo.readCountsOfTodoAchievedAtTheDay(readYear: shifetNow.year!, readMonth: shifetNow.month!) { value in
+//                self.countsOfTodoAchieved = value
+//                self.isProgressing = false
+//            }
         } else {
-            let shifetNow = Day.nowShiftedByYear(offset: pageOffset)
-            self.pageTitle = Day.toStringUpToYear(from: shifetNow)
-            FireTodo.readCountsOfTodoAchievedAtTheMonth(readYear: shifetNow.year!) { value in
-                self.countsOfTodoAchieved = value
-                self.isProgressing = false
-            }
+//            let shifetNow = Day.nowShiftedByYear(offset: pageOffset)
+//            self.pageTitle = Day.toStringUpToYear(from: shifetNow)
+//            FireTodo.readCountsOfTodoAchievedAtTheMonth(readYear: shifetNow.year!) { value in
+//                self.countsOfTodoAchieved = value
+//                self.isProgressing = false
+//            }
         }
     }
 }
