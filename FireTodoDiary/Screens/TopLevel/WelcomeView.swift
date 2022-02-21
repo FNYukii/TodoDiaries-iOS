66cc35a2e428eaf40867529231a6af8b4b5708a5
Yu357 <forestgreen14689@gmail.com>
2022-02-21 19:49:47 +0900

    [add] PageTabViewを配置した

--- FireTodoDiary/Screens/TopLevel/WelcomeView.swift
+++ FireTodoDiary/Screens/TopLevel/WelcomeView.swift
@@ -9,15 +9,31 @@ import SwiftUI
 
 struct WelcomeView: View {
     
-    @State var isShowSheet = false
+    @State private var isShowSheet = false
+    @State private var selection = 0
     
     var body: some View {
-        VStack {
-            Text("Welcome to Todo Diary")
-            Button("Sign in") {
-                isShowSheet.toggle()
+        
+        TabView(selection: $selection) {
+            VStack {
+                Text("Todoを追加しましょう")
             }
+            .tag(0)
+            VStack {
+                Text("Todoを達成し、履歴を振り返りましょう")
+            }
+            .tag(1)
+            VStack {
+                Text("統計情報を見ることもできます")
+                Button("sign_in_to_start") {
+                    isShowSheet.toggle()
+                }
+                .buttonStyle(BorderedProminentButtonStyle())
+            }
+            .tag(2)
+
         }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
         
         .sheet(isPresented: $isShowSheet) {
             FirebaseAuthView()
