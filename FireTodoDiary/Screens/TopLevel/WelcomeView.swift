6e4b9c8f44b925a6f9ffec19e8be342073164520
Yu357 <forestgreen14689@gmail.com>
2022-02-21 20:42:59 +0900

    [update] ボタンの位置を変更

--- FireTodoDiary/Screens/TopLevel/WelcomeView.swift
+++ FireTodoDiary/Screens/TopLevel/WelcomeView.swift
@@ -14,37 +14,41 @@ struct WelcomeView: View {
     
     var body: some View {
         
-        TabView(selection: $selection) {
-            VStack {
-                Image(decorative: "image01")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200)
-                Text("Todoを追加しましょう")
-            }
-            .tag(0)
-            VStack {
-                Image(decorative: "image02")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200)
-                Text("Todoを達成し、履歴を振り返りましょう")
-            }
-            .tag(1)
-            VStack {
-                Image(decorative: "image03")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200)
-                Text("統計情報を見ることもできます")
-                Button("sign_in_to_start") {
-                    isShowSheet.toggle()
+        VStack {
+            TabView(selection: $selection) {
+                VStack {
+                    Image(decorative: "image01")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 250)
+                    Text("Todoを追加しましょう")
                 }
-                .buttonStyle(BorderedProminentButtonStyle())
+                .tag(0)
+                VStack {
+                    Image(decorative: "image02")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 250)
+                    Text("Todoを達成し、履歴を振り返りましょう")
+                }
+                .tag(1)
+                VStack {
+                    Image(decorative: "image03")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 250)
+                    Text("統計情報を見ることもできます")
+                }
+                .tag(2)
             }
-            .tag(2)
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+            
+            Button("sign_in_to_start") {
+                isShowSheet.toggle()
+            }
+            .buttonStyle(BorderedProminentButtonStyle())
+            .opacity(selection == 2 ? 1 : 0)
         }
-        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
         
         .sheet(isPresented: $isShowSheet) {
             FirebaseAuthView()
