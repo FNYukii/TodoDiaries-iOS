1d3aaf26af3f4cd74686a425b10312465bd37e1a
Yu357 <forestgreen14689@gmail.com>
2022-02-21 16:17:17 +0900

    [add] FirstViewにProgressViewを配置

--- FireTodoDiary/Screens/TopLevel/FirstView.swift
+++ FireTodoDiary/Screens/TopLevel/FirstView.swift
@@ -18,21 +18,28 @@ struct FirstView: View {
         NavigationView {
             
             ZStack {
-                List {
-                    if pinnedTodosViewModel.todos.count != 0 {
-                        TodoSection(todos: pinnedTodosViewModel.todos, header: Text("pinned"))
-                    }
-                    if unpinnedTodosViewModel.todos.count != 0{
-                        TodoSection(todos: unpinnedTodosViewModel.todos, header: pinnedTodosViewModel.todos.count != 0 ? Text("others") : nil)
-                    }
-                }
                 
-                if pinnedTodosViewModel.todos.count == 0 && unpinnedTodosViewModel.todos.count == 0 && pinnedTodosViewModel.isLoaded && unpinnedTodosViewModel.isLoaded {
-                    VStack {
-                        Text("まだTodoはありません")
-                        Text("Todoを追加するとここに表示されます")
+                if !pinnedTodosViewModel.isLoaded || !unpinnedTodosViewModel.isLoaded {
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle())
+                } else {
+                    
+                    List {
+                        if pinnedTodosViewModel.todos.count != 0 {
+                            TodoSection(todos: pinnedTodosViewModel.todos, header: Text("pinned"))
+                        }
+                        if unpinnedTodosViewModel.todos.count != 0{
+                            TodoSection(todos: unpinnedTodosViewModel.todos, header: pinnedTodosViewModel.todos.count != 0 ? Text("others") : nil)
+                        }
                     }
-                    .foregroundColor(.secondary)
+                    
+                    if pinnedTodosViewModel.todos.count == 0 && unpinnedTodosViewModel.todos.count == 0 {
+                        VStack {
+                            Text("まだTodoはありません")
+                            Text("Todoを追加するとここに表示されます")
+                        }
+                        .foregroundColor(.secondary)
+                    }
                 }
             }
             
