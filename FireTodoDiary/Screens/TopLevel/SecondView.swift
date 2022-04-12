30ab9bd2f1c702aacf2dae92959404ebe2278aea
Yu357 <forestgreen14689@gmail.com>
2022-04-12 21:05:26 +0900

    feat: ボタンを押すことで、前の月の達成済みTodoを表示するようにした

--- FireTodoDiary/Screens/TopLevel/SecondView.swift
+++ FireTodoDiary/Screens/TopLevel/SecondView.swift
@@ -9,14 +9,16 @@ import SwiftUI
 
 struct SecondView: View {
     
-    var days: [Int] = []
+    @State private var days: [Int] = []
+    @State private var monthOffset = 0
     
     init() {
         // 今月の年と月
         let now = Calendar.current.dateComponents(in: .current, from: Date())
         let nowYear = now.year!
         let nowMonth = now.month!
-        self.days = Day.daysAtTheMonth(year: nowYear, month: nowMonth)
+        let daysAtCurrentMonth = Day.daysAtTheMonth(year: nowYear, month: nowMonth)
+        _days = State(initialValue: daysAtCurrentMonth)
     }
     
     var body: some View {
@@ -25,6 +27,14 @@ struct SecondView: View {
             List {
                 ForEach(days, id: \.self){ achievedDay in
                     DailyAchievedTodosSection(achievedDay: achievedDay)
+                }
+                Button(action: {
+                    monthOffset -= 1
+                    let yearAndMonthShifted = Day.nowShiftedByMonth(offset: monthOffset)
+                    let daysAtPrevMonth = Day.daysAtTheMonth(year: yearAndMonthShifted.year!, month: yearAndMonthShifted.month!)
+                    days.append(contentsOf: daysAtPrevMonth)
+                }) {
+                    Text("load_prev_month")
                 }
             }
             
