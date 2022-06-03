510c7cf93e4c055f09a050f8d90843296afd0cd4
Yu357 <forestgreen14689@gmail.com>
2022-06-03 20:39:54 +0900

    feat: 未達成Todoの読み込み時にProgressViewを表示しないようにした

--- FireTodoDiary/Screens/TopLevel/FirstView.swift
+++ FireTodoDiary/Screens/TopLevel/FirstView.swift
@@ -19,27 +19,21 @@ struct FirstView: View {
             
             ZStack {
                 
-                if !pinnedTodosViewModel.isLoaded || !unpinnedTodosViewModel.isLoaded {
-                    ProgressView()
-                        .progressViewStyle(CircularProgressViewStyle())
-                } else {
-                    
-                    List {
-                        if pinnedTodosViewModel.todos.count != 0 {
-                            UnachievedTodosSection(todos: pinnedTodosViewModel.todos, header: Text("pinned"))
-                        }
-                        if unpinnedTodosViewModel.todos.count != 0{
-                            UnachievedTodosSection(todos: unpinnedTodosViewModel.todos, header: pinnedTodosViewModel.todos.count != 0 ? Text("others") : nil)
-                        }
+                List {
+                    if pinnedTodosViewModel.todos.count != 0 {
+                        UnachievedTodosSection(todos: pinnedTodosViewModel.todos, header: Text("pinned"))
                     }
-                    
-                    if pinnedTodosViewModel.todos.count == 0 && unpinnedTodosViewModel.todos.count == 0 {
-                        VStack {
-                            Text("there_is_no_todo_yet")
-                            Text("when_you_create_a_todo_it_will_appear_here")
-                        }
-                        .foregroundColor(.secondary)
+                    if unpinnedTodosViewModel.todos.count != 0{
+                        UnachievedTodosSection(todos: unpinnedTodosViewModel.todos, header: pinnedTodosViewModel.todos.count != 0 ? Text("others") : nil)
                     }
+                }
+                
+                if pinnedTodosViewModel.todos.count == 0 && unpinnedTodosViewModel.todos.count == 0 && pinnedTodosViewModel.isLoaded && unpinnedTodosViewModel.isLoaded {
+                    VStack {
+                        Text("there_is_no_todo_yet")
+                        Text("when_you_create_a_todo_it_will_appear_here")
+                    }
+                    .foregroundColor(.secondary)
                 }
             }
             
