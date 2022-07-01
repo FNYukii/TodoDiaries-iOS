103a8643b31afcb7b94bbf194b7952071667d75a
Yu357 <forestgreen14689@gmail.com>
2022-07-01 21:08:36 +0900

    clean: 未達成Todosのmove関数内のコードを、FireTodoクラスへ移動

--- TodoDiaries/Utilities/FireTodo.swift
+++ TodoDiaries/Utilities/FireTodo.swift
@@ -206,6 +206,45 @@ class FireTodo {
             }
     }
     
+    static func moveTodos(todos: [Todo], sourceIndexSet: IndexSet, destination: Int) {
+        // 移動元と移動先のindexを取得
+        let from = Int(sourceIndexSet.first!)
+        var destination = destination
+        if from < destination {
+            destination -= 1
+        }
+        
+        if from > destination {
+            // Todoを上に移動
+            let movedTodo = todos[from]
+            var newOrder = 0.0
+            if destination == 0 {
+                let minOrder = todos.first!.order!
+                newOrder = minOrder - 100
+            } else {
+                let prevOrder = todos[destination - 1].order!
+                let nextOrder = todos[destination].order!
+                newOrder = (prevOrder + nextOrder) / 2
+            }
+            FireTodo.updateTodo(id: movedTodo.id, order: newOrder)
+        }
+        
+        if from < destination {
+            // Todoを下に移動
+            let movedTodo = todos[from]
+            var newOrder = 0.0
+            if destination == todos.count - 1 {
+                let maxOrder = todos.last!.order!
+                newOrder = maxOrder + 100
+            } else {
+                let prevOrder = todos[destination].order!
+                let nextOrder = todos[destination + 1].order!
+                newOrder = (prevOrder + nextOrder) / 2
+            }
+            FireTodo.updateTodo(id: movedTodo.id, order: newOrder)
+        }
+    }
+    
     static func pinTodo(id: String) {
         // pinnedTodosのorder最大値を読み取り
         readMaxOrder(isPinned: true) { maxOrder in
