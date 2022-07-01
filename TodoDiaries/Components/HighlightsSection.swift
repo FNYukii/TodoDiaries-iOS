9181cc6071cf4c95cdcd65d4b7b9edc3a3674ac0
Yu357 <forestgreen14689@gmail.com>
2022-07-01 11:55:43 +0900

    feat: HighlightsSectionをリファクタリング

--- TodoDiaries/Components/HighlightsSection.swift
+++ TodoDiaries/Components/HighlightsSection.swift
@@ -9,12 +9,13 @@ import SwiftUI
 
 struct HighlightsSection: View {
     
+    @State private var message = ""
+    
     @State private var value0 = 0.0
     @State private var value1 = 0.0
     private let xAxisLabels: [String]
-    @State private var message = ""
     
-    @State private var isProgressing = true
+    @State private var isLoaded = false
     
     init() {
         let xAxisLabel0 = NSLocalizedString("yesterday", comment: "")
@@ -24,48 +25,50 @@ struct HighlightsSection: View {
     
     var body: some View {
         Section(header: Text("highlights")) {
-            VStack(alignment: .leading) {
-                if isProgressing {
-                    ProgressView()
-                        .progressViewStyle(CircularProgressViewStyle())
-                        .frame(maxWidth: .infinity, alignment: .center)
-                        .frame(height: 170)
-                }
-                if !isProgressing {
-                    Text(message)
-                        .fixedSize(horizontal: false, vertical: true)
-                    HorizontalBarChart(value0: value0, value1: value1, xAxisLabels: xAxisLabels)
-                        .frame(height: 170)
-                }
+            // ProgressView
+            if !isLoaded {
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle())
+                    .frame(maxWidth: .infinity, alignment: .center)
             }
-            .animation(.default, value: message)
+            
+            if isLoaded {
+                VStack {
+                    // Message
+                    if value0 == value1 {
+                        Text("the_count_of_todos_achieved_today_is_the_same_as_yesterday")
+                            .fixedSize(horizontal: false, vertical: true)
+                    } else if value0 > value1 {
+                        Text("the_count_of_todos_achieved_today_is_less_than_yesterday")
+                            .fixedSize(horizontal: false, vertical: true)
+                    } else {
+                        Text("the_count_of_todos_achieved_today_is_higher_than_yesterday")
+                            .fixedSize(horizontal: false, vertical: true)
+                    }
+
+                    // BarChart
+                    HorizontalBarChart(value0: value0, value1: value1, xAxisLabels: xAxisLabels)
+                        .frame(height: 150)
+                }
+                .frame(height: 250)
+                .animation(.default, value: message)
+            }
         }
         
         .onAppear {
-            // 昨日と今日
-            let now = Date()
-            let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: now)!
-            
             // 昨日のTodo達成数
+            let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: Date())!
             let yesterdayComponents = Calendar.current.dateComponents(in: TimeZone.current, from: yesterday)
             FireTodo.readAchieveCountAtDay(year: yesterdayComponents.year!, month: yesterdayComponents.month!, day: yesterdayComponents.day!) { countAtYesterday in
                 value0 = Double(countAtYesterday)
-                
-                // 今日のTodo達成数
-                let todayComponents = Calendar.current.dateComponents(in: TimeZone.current, from: Date())
-                FireTodo.readAchieveCountAtDay(year: todayComponents.year!, month: todayComponents.month!, day: todayComponents.day!) { countAtToday in
-                    value1 = Double(countAtToday)
-                    
-                    // message
-                    if value0 == value1 {
-                        message = NSLocalizedString("the_count_of_todos_achieved_today_is_the_same_as_yesterday", comment: "")
-                    } else if value0 > value1 {
-                        message = NSLocalizedString("the_count_of_todos_achieved_today_is_less_than_yesterday", comment: "")
-                    } else {
-                        message = NSLocalizedString("the_count_of_todos_achieved_today_is_higher_than_yesterday", comment: "")
-                    }
-                    isProgressing = false
-                }
+                isLoaded = true
+            }
+            
+            // 今日のTodo達成数
+            let todayComponents = Calendar.current.dateComponents(in: TimeZone.current, from: Date())
+            FireTodo.readAchieveCountAtDay(year: todayComponents.year!, month: todayComponents.month!, day: todayComponents.day!) { countAtToday in
+                value1 = Double(countAtToday)
+                isLoaded = true
             }
         }
     }
